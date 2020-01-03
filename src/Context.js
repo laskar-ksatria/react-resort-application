5028f3c48ea0d801822e60bd787d1b0287034717
laskar-ksatria <laskar-ksatria@gmail.com>
2020-01-03 12:12:37 +0700

    continue-course-II

--- src/Context.js
+++ src/Context.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 
-const RoomContext =React.createContext();
+const RoomContext = React.createContext()
 
 class RoomProvider extends Component {
-
     state = {
-        
+
     }
 
     render() {
         return (
-            <div>
-
-            </div>
+            <RoomContext.Provider value="hello">
+                {this.props.children}
+            </RoomContext.Provider>
         )
     }
 }
 
+const RoomConsumer = RoomContext.Consumer;
 
-export default RoomProvider;
+export { RoomProvider, RoomConsumer, RoomContext };
